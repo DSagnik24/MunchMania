42445635eece7e8af58abb507968784ae4fca3af
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-12-21 20:47:37 +0530

    First commit

---
+++ src/main/java/com/sagnik/foodapp_api/FoodappApiApplication.java
@@ -0,0 +1,13 @@
+package com.sagnik.foodapp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagnik/foodapp_api/FoodappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagnik.foodapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
